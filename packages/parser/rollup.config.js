550778735bd86b52fae272f5b8aaab9daa1f35a8
Richard Foster <richard@rpf.me>
2021-04-19 20:07:49 +0100

    Compile and expose parser typescript declarations

--- packages/parser/rollup.config.js
+++ packages/parser/rollup.config.js
@@ -2,28 +2,56 @@ import path from 'path'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
-export default {
-  input: path.join('src', 'index.ts'),
-  output: [
-    {
-      dir: path.join('dist', 'esm'),
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      dir: path.join('dist', 'commonjs'),
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      dir: path.join('dist', 'umd'),
-      format: 'umd',
-      name: 'ComixParser',
-      sourcemap: true,
-    }
-  ],
-  plugins: [
-    nodeResolve(),
-    typescript({ module: 'esnext' }),
-  ]
-}
+export default [
+  {
+    input: path.join('src', 'index.ts'),
+    output: [
+      {
+        dir: path.join('dist', 'esm'),
+        format: 'esm',
+        sourcemap: true,
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      typescript({
+        module: 'esnext',
+        declaration: true,
+        declarationDir: path.join('dist', 'esm', 'types')
+      }),
+    ],
+  },
+  {
+    input: path.join('src', 'index.ts'),
+    output: [
+      {
+        dir: path.join('dist', 'commonjs'),
+        format: 'cjs',
+        sourcemap: true,
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      typescript({
+        module: 'esnext',
+        declaration: true,
+        declarationDir: path.join('dist', 'commonjs', 'types')
+      }),
+    ],
+  },
+  {
+    input: path.join('src', 'index.ts'),
+    output: [
+      {
+        dir: path.join('dist', 'umd'),
+        format: 'umd',
+        name: 'ComixParser',
+        sourcemap: true,
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      typescript({ module: 'esnext' }),
+    ],
+  }
+]
