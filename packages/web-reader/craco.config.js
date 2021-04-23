9a41d8a7f49736811f929a1d35fdbfcaeab36ac8
Richard Foster <richard@rpf.me>
2021-04-23 22:15:33 +0100

    Move jest config hack into craco config too

--- packages/web-reader/craco.config.js
+++ packages/web-reader/craco.config.js
@@ -5,5 +5,14 @@ module.exports = {
     alias: {
       react: path.resolve(__dirname, 'node_modules', 'react'),
     },
+  },
+  jest: {
+    config: (existing) => ({
+      ...existing,
+      setupFilesAfterEnv: [
+        ...(existing.setupFilesAfterEnv || []),
+        path.join(__dirname, 'src', 'setupTests.ts')
+      ]
+    })
   }
 }
