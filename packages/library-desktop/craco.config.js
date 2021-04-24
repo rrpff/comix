0196a908727c229d1d4309132557cc4f12ba2b3c
Richard Foster <richard@rpf.me>
2021-04-24 14:57:38 +0100

    Set up server tests

--- packages/library-desktop/craco.config.js
+++ packages/library-desktop/craco.config.js
@@ -7,12 +7,21 @@ module.exports = {
     },
   },
   jest: {
-    config: (existing) => ({
+    configure: (existing) => ({
       ...existing,
+      roots: [
+        ...existing.roots,
+        path.join(__dirname, 'server'),
+      ],
+      testMatch: [
+        ...existing.testMatch,
+        path.join(__dirname, 'server', '__tests__', '**', '*.{js,jsx,ts,tsx}'),
+        path.join(__dirname, 'server', '**', '*.{spec,test}.{js,jsx,ts,tsx}'),
+      ],
       setupFilesAfterEnv: [
         ...(existing.setupFilesAfterEnv || []),
-        path.join(__dirname, 'src', 'setupTests.ts')
-      ]
+        path.join(__dirname, 'src', 'setupTests.ts'),
+      ],
     })
   }
 }
