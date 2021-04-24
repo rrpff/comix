cf2a94f053107a1bc2bc5971116ab0fa36dd10cc
Richard Foster <richard@rpf.me>
2021-04-24 17:51:01 +0100

    Bind own methods to ServiceIpc

--- packages/library-desktop/server/lib/ServiceIpc.ts
+++ packages/library-desktop/server/lib/ServiceIpc.ts
@@ -22,46 +22,47 @@ export interface ServiceLogger {
 
 export class ServiceIpc<TServiceMap extends ServiceMap = ServiceMap> {
   private services: Map<keyof(TServiceMap), Service<any>> = new Map()
-  private logger?: ServiceLogger = null
+  private logger?: ServiceLogger
 
-  public log(logger: ServiceLogger) {
+  public log = (logger: ServiceLogger) => {
+    console.log('added logger :-)')
     this.logger = logger
   }
 
-  public async use<T, K extends keyof(TServiceMap)>(service: K, handler: Service<T>) {
+  public use = async <T, K extends keyof(TServiceMap)>(service: K, handler: Service<T>) => {
     this.services.set(service, handler)
   }
 
-  public async handle<K extends keyof(TServiceMap)>(
+  public handle = async <K extends keyof(TServiceMap)>(
     ipcEvent: ServiceIpcEvent,
     requestId: string,
     serviceName: K,
     ...args: ArgumentTypes<TServiceMap[K]>
-  ): Promise<void> {
+  ): Promise<void> => {
     if (!ipcEvent) throw new Error('No ipcEvent given')
     if (!ipcEvent.sender) throw new Error('No sender given')
     if (!requestId) throw new Error('No request ID given')
     if (!serviceName) throw new Error('No service given')
 
-    this.logRequest(requestId, serviceName, args)
+    this.logRequest(requestId, serviceName as string, args)
 
     const service = this.services.get(serviceName)
 
     if (service === undefined) {
       this.logger?.error?.call(this.logger, 'ipc-response', requestId, serviceName, 'error')
-      this.logResponse(requestId, serviceName, false)
+      this.logResponse(requestId, serviceName as string, false)
       return ipcEvent.sender.send('ipc-response', requestId, { error: `Invalid service: ${serviceName}` })
     }
 
     const response = await service(...args)
-    this.logResponse(requestId, serviceName, Object.keys(response).includes('success'))
+    this.logResponse(requestId, serviceName as string, Object.keys(response).includes('success'))
     ipcEvent.sender.send('ipc-response', requestId, response)
   }
 
-  private logRequest = (requestId, serviceName, args) =>
+  private logRequest = (requestId: string, serviceName: string, args: any[]) =>
     this.logger?.info?.call(this.logger, 'ipc-request', requestId, serviceName, args)
 
-  private logResponse = (requestId, serviceName, successful) => {
+  private logResponse = (requestId: string, serviceName: string, successful: boolean) => {
     const message = successful ? 'success' : 'error'
     const func = successful ? this.logger?.info : this.logger?.error
     func?.call(this.logger, 'ipc-response', requestId, serviceName, message)
