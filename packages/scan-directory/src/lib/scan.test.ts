30935c95e359c017ab18c048c0ade02baedc1be6
Richard Foster <richard@rpf.me>
2021-04-25 04:50:04 +0100

    Remove unnecessary describe blocks

--- packages/scan-directory/src/lib/scan.test.ts
+++ packages/scan-directory/src/lib/scan.test.ts
@@ -1,29 +1,26 @@
-import path from 'path'
 import { scan } from './scan'
 import { fixturePath } from '../../test/helpers'
 
-describe('scan', () => {
-  it('returns the paths of all CBR and CBZ files in a directory as absolute paths', async () => {
-    const results = await scan(fixturePath('folder'))
-    expect(results).toMatchObject([{
-      path: fixturePath('folder', 'another-fake-file.cbr'),
-    }])
-  })
+it('returns the paths of all CBR and CBZ files in a directory as absolute paths', async () => {
+  const results = await scan(fixturePath('folder'))
+  expect(results).toMatchObject([{
+    path: fixturePath('folder', 'another-fake-file.cbr'),
+  }])
+})
 
-  it('returns last modified times', async () => {
-    const results = await scan(fixturePath('folder'))
-    expect(results).toMatchObject([{
-      lastModified: 1619311097052.7192
-    }])
-  })
+it('returns last modified times', async () => {
+  const results = await scan(fixturePath('folder'))
+  expect(results).toMatchObject([{
+    lastModified: 1619311097052.7192
+  }])
+})
 
-  it('scans recursively', async () => {
-    const results = await scan(fixturePath('..'))
-    expect(results).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ path: fixturePath('fake-comic-file.cbz') }),
-        expect.objectContaining({ path: fixturePath('folder', 'another-fake-file.cbr') }),
-      ])
-    )
-  })
+it('scans recursively', async () => {
+  const results = await scan(fixturePath('..'))
+  expect(results).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({ path: fixturePath('fake-comic-file.cbz') }),
+      expect.objectContaining({ path: fixturePath('folder', 'another-fake-file.cbr') }),
+    ])
+  )
 })
