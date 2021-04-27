363ef1e02a65c7e7fee79a1a8106bd0eb04ea7c0
Richard Foster <richard@rpf.me>
2021-04-27 18:36:23 +0100

    Set up library when starting API

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,13 @@
-import server from './server'
+import { homedir } from 'os'
+import path from 'path'
+import { FileLibraryConfig, Library } from '@comix/library'
+import createServer from './server'
 
 const PORT = process.env.PORT || 4000
+
+const configPath = path.join(homedir(), '.comix', 'library.db')
+const library = new Library(new FileLibraryConfig(configPath))
+const server = createServer({ library })
 
 server.listen(PORT, () => {
   console.log(`API running at http://localhost${PORT}`)
--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,21 +1,29 @@
 import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
+import { Library } from '@comix/library'
 import schema from './schema'
 import { GraphqlContext } from './types'
 
-const app = express()
-
-const context = (): GraphqlContext => {
-  return {
-    loaders: {}
-  }
+interface ServerOptions {
+  library: Library
 }
 
-const apollo = new ApolloServer({
-  schema,
-  context
-})
+export default ({ library }: ServerOptions) => {
+  const app = express()
 
-apollo.applyMiddleware({ app, path: '/graphql' })
+  const context = (): GraphqlContext => {
+    return {
+      library,
+      loaders: {},
+    }
+  }
 
-export default app
+  const apollo = new ApolloServer({
+    schema,
+    context
+  })
+
+  apollo.applyMiddleware({ app, path: '/graphql' })
+
+  return app
+}
