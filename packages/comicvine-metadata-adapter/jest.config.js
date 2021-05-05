a88f6ad179abb19f1de90adc887cfa02508a231c
Richard Foster <richard@rpf.me>
2021-05-05 18:52:34 +0100

    Only run tests in src

--- packages/comicvine-metadata-adapter/jest.config.js
+++ packages/comicvine-metadata-adapter/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  watchPathIgnorePatterns: ['node_modules', 'test/fixtures/outputs']
+  watchPathIgnorePatterns: ['node_modules', 'test/fixtures/outputs'],
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
+    "<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)",
+  ]
 };
