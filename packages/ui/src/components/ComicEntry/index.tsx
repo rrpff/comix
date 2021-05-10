f14a57aa11d31589298ecc3c9a869ca2c6fa3058
Richard Foster <richard@rpf.me>
2021-05-10 19:11:31 +0100

    Improve styling of reading progress on comic entries

--- packages/ui/src/components/ComicEntry/index.tsx
+++ packages/ui/src/components/ComicEntry/index.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
-import React, { useState } from 'react'
+import React, { ForwardedRef, useState } from 'react'
 import Measure from 'react-measure'
+import { FaCheck } from 'react-icons/fa'
 import { ProgressBar } from '../ProgressBar'
 import { Sentence } from '../LoadableText'
 import { LoadingBox } from '../LoadingBox'
@@ -37,8 +38,17 @@ export const ComicEntry = ({
         <Container onClick={onClick} style={{ width: imageSize.width }} data-testid={title}>
           {loading
             ? <LoadingBox width="100%" height="240px" />
-            : <img ref={measureRef} src={imageUrl} alt={title} />
+            : <CoverImage finished={readingProgress === 1.0} ref={measureRef} src={imageUrl} alt={title} />
           }
+
+          <Progress style={{ top: imageSize.height - 8 + 10 }}>
+            <ProgressBar
+              progress={readingProgress}
+              barStyles={{
+                background: '#3742FA'
+              }}
+            />
+          </Progress>
 
           <strong>
             <Sentence loading={loading}>{() => title}</Sentence>
@@ -49,32 +59,54 @@ export const ComicEntry = ({
               <Sentence loading={loading}>{() => subtitle}</Sentence>
             </span>
           )}
-
-          {readingProgress ? (
-            <Progress style={{ top: imageSize.height - 8 + 10 }}>
-              <ProgressBar
-                progress={readingProgress}
-                barStyles={{
-                  background: '#fff'
-                }}
-              />
-            </Progress>
-          ) : null}
         </Container>
       }
     </Measure>
   )
 }
 
-const Progress = styled.div`
+const CoverImage = React.forwardRef((props: { src: string, alt: string, finished: boolean }, ref: ForwardedRef<HTMLImageElement>) => {
+  return (
+    <div style={{ position: 'relative' }}>
+      <img src={props.src} alt={props.alt} ref={ref} className="cover-image" />
+      <Overlay visible={props.finished} className="cover-overlay">
+        <FaCheck />
+        Finished
+      </Overlay>
+    </div>
+  )
+})
+
+const Overlay = styled.div<{ visible: boolean }>`
   position: absolute;
-  width: calc(100% - 60px);
-  left: 30px;
-  height: 8px;
-  border-radius: 8px;
-  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.75), 0px 0px 5px rgba(0, 0, 0, 0.75);
-  background: rgba(0, 0, 0, 0.8);
+  left: 0px;
+  top: 0px;
+  height: 240px;
+  width: 100%;
+  background: #000;
+  opacity: ${props => props.visible ? 0.7 : 0};
+  z-index: 30;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-size: 1.4rem;
+
+  svg {
+    font-size: 2.4rem;
+    margin-bottom: 10px;
+  }
+`
+
+const Progress = styled.div`
+  height: 4px;
+  border-radius: 3px;
+  margin-top: 6px;
+  background: rgba(0, 0, 0, 0.1);
   overflow: hidden;
+  z-index: 10;
 `
 
 const Container = styled.section`
@@ -88,17 +120,28 @@ const Container = styled.section`
   text-align: center;
 
   &:hover {
-    img {
+    .cover-image,
+    .cover-overlay {
       transform: perspective(500px) rotateX(7deg) translateY(-10px) scale(1.05);
       box-shadow: 0px 12px 20px rgba(0, 0, 0, 0.3), 0px 8px 12px rgba(0, 0, 0, 0.2);
     }
+
+    .cover-overlay {
+      opacity: 0;
+    }
   }
 
-  img {
+  .cover-image {
+    position: relative;
     align-self: center;
     height: 240px;
     box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);
-    transition: transform 0.4s, box-shadow 0.4s;
+    z-index: 20;
+  }
+
+  .cover-image,
+  .cover-overlay {
+    transition: opacity 0.2s, transform 0.4s, box-shadow 0.4s;
   }
 
   strong {
